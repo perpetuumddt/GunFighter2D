c2d88db434a51a5f53477a6af0380405571be07b
perpetuumddt <102897989+perpetuumddt@users.noreply.github.com>
2023-06-25 23:20:31 +0300

    added wave data

---
+++ Assets/Scripts/ScriptableObjects/Data/Wave/WaveData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave Data", menuName = "Data/Level Data/New Wave Data")]
+
+public class WaveData : ScriptableObject
+{
+    [SerializeField]
+    private GameObject[] _enemyPrefabs;
+    public GameObject[] EnemyPrefabs => _enemyPrefabs;
+
+    [SerializeField]
+    private int[] _enemyCount;
+    public int[] EnemyCount => _enemyCount;
+
+    [SerializeField]
+    private float _spawnRate;
+    public float SpawnRate => _spawnRate;
+
+    [SerializeField]
+    private float _waveDuration;
+    public float WaveDuration => _waveDuration;
+
+    [SerializeField]
+    private float _difficultyModifier;
+    public float DifficultyModifier => _difficultyModifier;
+}
