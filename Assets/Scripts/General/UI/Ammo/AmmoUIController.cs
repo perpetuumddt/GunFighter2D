b9c2944e538e461805d517fd0d7a1089e90309b9
perpetuumddt <102897989+perpetuumddt@users.noreply.github.com>
2023-06-28 14:58:41 +0300

    Created events in PlayerAttackController

--- Assets/Scripts/General/UI/Ammo/AmmoUIController.cs
+++ Assets/Scripts/General/UI/Ammo/AmmoUIController.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class AmmoUIController : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject _ammoUnitPrefab;
+
+    [SerializeField]
+    private PlayerAttackController _playerAttackController;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void UpdateAmmo()
+    {
+        
+    }
+
+    private void SetupAmmo()
+    {
+
+    }
 }
