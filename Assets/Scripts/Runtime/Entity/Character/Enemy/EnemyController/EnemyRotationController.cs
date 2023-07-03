5c70222e9068c7c971ad44f8997665023eef5822
perpetuumddt <102897989+perpetuumddt@users.noreply.github.com>
2023-07-03 22:35:15 +0300

    Made rotation controller abstract, added EnemyRotationController

---
+++ Assets/Scripts/Runtime/Entity/Character/Enemy/EnemyController/EnemyRotationController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Gunfighter.Runtime.Entity.Character.Controller;
+using UnityEngine;
+
+namespace Gunfighter.Runtime.Entity.Character.Enemy.EnemyController
+
+{
+    public class EnemyRotationController : CharacterRotationController
+    {
+        public override void CheckLookingDirection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Vector2 CheckMovementDirection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CheckRollingDirection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Flip()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/Runtime/Entity/Character/Player/PlayerController/PlayerRotationController.cs
+++ Assets/Scripts/Runtime/Entity/Character/Player/PlayerController/PlayerRotationController.cs
@@ -1,10 +1,59 @@
 using Gunfighter.Runtime.Entity.Character.Controller;
+using Gunfighter.Runtime.Entity.Character.Player.Components;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Gunfighter.Runtime.Entity.Character.Player.PlayerController
 {
     public class PlayerRotationController : CharacterRotationController
     {
-    
-   
+        protected bool LookLeft, RollLeft;
+        private Camera _mainCam;
+        private PlayerInputHandler _playerInputHandler;
+        
+        private void Start()
+        {
+            _playerInputHandler = GetComponent<PlayerInputHandler>();
+            _mainCam = FindObjectOfType<Camera>();
+        }
+
+        public override void CheckLookingDirection()
+        {
+            Vector3 mousePos = _mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 rotation = mousePos - transform.position;
+
+            if (!LookLeft && rotation.x > 0.1f)
+            {
+                Flip();
+            }
+            if (LookLeft && rotation.x < -0.1f)
+            {
+                Flip();
+            }
+        }
+
+        public override Vector2 CheckMovementDirection()
+        {
+            return _playerInputHandler.MovementInputVector;
+        }
+        
+        public override void CheckRollingDirection()
+        {
+            if (!RollLeft && CheckMovementDirection().x > 0.1f)
+            {
+                Flip();
+            }
+            if (RollLeft && CheckMovementDirection().x < -0.1f)
+            {
+                Flip();
+            }
+        }
+
+        protected override void Flip()
+        {
+            LookLeft = !LookLeft;
+            RollLeft = !RollLeft;
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
